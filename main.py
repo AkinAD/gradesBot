32da8e462e51813da9d5417c806122ccc6798dfa
AkinAD <mrspence004@gmail.com>
2019-12-27 23:44:21 -0500

    Working state

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
-import os
+from datetime import datetime
 import time
+# import texteditor
+import configparser
+from utility_methods.utility_methods import *
+import urllib.request
 
 #Have a way of storing the report to a text file and find a way to compare the
 # result you just returned to the last time you ran the code to see if there are new grades or nah
@@ -11,9 +15,35 @@ class gradesBot:
 		self.password = config['PPY_AUTH']['PASSWORD']
 
 		self.all_grades_url = config['PPY_GRADE_URLS']['FULL_LIST']
-		self.all_grades_url = config['PPY_GRADE_URLS']['CURR_YEAR']
+		self.curr_year_url = config['PPY_GRADE_URLS']['CURR_YEAR']
 
-		self.driver = webdriver.Chrome(config_file_path['ENVIRONMENT']['CHROMEDRIVER_PATH']
+		self.driver = webdriver.Chrome(config['ENVIRONMENT']['CHROMEDRIVER_PATH'])
+
+	def login(self):
+		self.driver.get(self.all_grades_url)
+
+		login_btn = self.driver.find_element_by_xpath("//input[@value='Login']")
+		username_input =  self.driver.find_element_by_xpath("//input[@name='mli']")
+		password_input = self.driver.find_element_by_xpath("//input[@name='password']")
+
+		username_input.send_keys(self.username)
+		password_input.send_keys(self.password)
+		login_btn.click()
+		time.sleep(3)
+
+	def getGrades(self):
+		self.tableOfGrades = self.driver.find_elements_by_xpath("//table[@class='bodytext']")[0]
+		
+		now = datetime.now() # current date and time
+		date_time = now.strftime("%A %B %d %I:%M %P")
+		filename = 'Grades_checked_on_{}.txt'.format(date_time)
+		f = open(filename, "w+")
+		for i in self.tableOfGrades.find_elements_by_xpath(".//tr"):
+			f.write('{}\n'.format(i.text))
+			print('{}'.format(i.text))
+		f.close()
+		#text = texteditor.open(filename) """ automatically open the file afterwards"""
+	
 
 if __name__ == '__main__':
 	config_file_path = './config.ini'
@@ -23,3 +53,4 @@ if __name__ == '__main__':
 
 	bot = gradesBot()
 	bot.login()
+	bot.getGrades()
